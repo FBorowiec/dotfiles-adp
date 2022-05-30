caa61655a437194beac19f0e225bd6a1adcb5980
Franek Borowiec <franek.borowiec@gmail.com>
2022-05-30 13:01:39 +0200

    Cht.sh - added more core utils and languages

--- scripts/.config/cht.sh
+++ scripts/.config/cht.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
-
-languages=`echo "cpp c lua python shell" | tr ' ' '\n'`
-core_utils=`echo "xargs find mv cp sed awk curl tar rg" | tr ' ' '\n'`
-
-selected=`printf "$languages\n$core_utils" | fzf`
+selected=`cat ~/.tmux-cht-languages ~/.tmux-cht-command | fzf`
 if [[ -z $selected ]]; then
     exit 0
 fi
-read -p "Query: " query
 
-if printf $languages | grep -qs $selected; then
-    tmux neww bash -c "curl cht.sh/$selected/`echo $query | tr ' ' '+'` & while [ : ]; do sleep 1; done"
+read -p "Enter Query: " query
+
+if grep -qs "$selected" ~/.tmux-cht-languages; then
+    query=`echo $query | tr ' ' '+'`
+    tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
 else
-    tmux neww bash -c "curl cht.sh/$selected+$query & while [ : ]; do sleep 1; done"
+    tmux neww bash -c "curl -s cht.sh/$selected~$query | less"
 fi
